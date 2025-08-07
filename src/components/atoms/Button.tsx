7ced60a8590801d1114f14bf1708abf82ea60ae5
GuiTorres3000 <guilhermetorres3000@gmail.com>
2025-08-06 22:37:48 -0300

    docs: cva description in comments

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -2,11 +2,12 @@ import { type VariantProps } from "class-variance-authority";
 import { buttonVariants } from "./buttonVariants";
 import Text from "./Text";
 
+// Usa todas as props padrões de <button>, exceto "size" e "disabled"
 interface ButtonProps extends Omit<React.ComponentProps<"button">, "size" | "disabled">,
-      VariantProps<typeof buttonVariants> { }
+      VariantProps<typeof buttonVariants> { } // Adiciona os variants definidos no botão
 
 export default function Button({ variant, size, disabled, className, children, ...props }: ButtonProps) {
-      <button className={buttonVariants({variant, size, disabled, className})} {...props}>
+      <button className={buttonVariants({ variant, size, disabled, className })} {...props}>
             <Text variant="body-md-bold">
                   {children}
             </Text>
--- src/components/atoms/Text.tsx
+++ src/components/atoms/Text.tsx
@@ -1,10 +1,14 @@
 import { createElement, type ReactNode, type JSX } from "react";
+// Importa tipos da biblioteca de variantes
 import { type VariantProps } from "class-variance-authority";
+// Importa os estilos de texto
 import { textVariants } from "./textVariants";
 
+// Define as props que o componente Text pode receber
 interface TextProps extends VariantProps<typeof textVariants>{
       // Qualquer chave de IntrisicElements (que mapeia todas tags HTML para tipo React)
       as?: keyof JSX.IntrinsicElements;
+      // Permite passar classes adicionais
       className?: string;
       // ReactNode representa qualquer coisa que React possa renderizar
       children?: ReactNode;
