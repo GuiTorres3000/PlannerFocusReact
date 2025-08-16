5a2f98ed41ec8dacc43292fbc7777addb295312c
GuiTorres3000 <guilhermetorres3000@gmail.com>
2025-08-15 21:47:07 -0300

    feat: interactive task item

--- src/components/molecules/TaskItem.tsx
+++ src/components/molecules/TaskItem.tsx
@@ -4,17 +4,38 @@ import Container from "../atoms/Container/Container";
 import InputCheckbox from "../atoms/InputCheckbox/InputCheckbox";
 import Text from "../atoms/Text/Text";
 import Button from "../atoms/Button/Button";
-import { FaTrash, FaPencilAlt } from "react-icons/fa";
+import { FaTrash, FaPencilAlt, FaUndo, FaCheck } from "react-icons/fa";
+import { useState } from "react";
+import InputText from "../atoms/InputText/InputText";
 
 export default function TaskItem() {
+
+      const [isEditing, setIsEditing] = useState(false);
+
+      function handleEditTask(){
+            setIsEditing(!isEditing)
+      }
+
       return (
             <Card size="md" className="flex items-center gap-3">
-                        <InputCheckbox />
-                        <Text className="flex-1">Fazer Compras da Semana</Text>
-                  <Container as="div" className="flex gap-1">
-                        <Button variant="icon_secondary" size="icon"><FaTrash /></Button>
-                        <Button variant="icon_primary" size="icon"><FaPencilAlt /></Button>
-                  </Container>
+                  {isEditing ?
+                        <>
+                              <InputText className="flex-1"/>
+                              <Container as="div" className="flex gap-1">
+                                    <Button variant="icon_secondary" size="icon" onClick={handleEditTask}><FaUndo /></Button>
+                                    <Button variant="icon_primary" size="icon"><FaCheck /></Button>
+                              </Container>
+                        </>
+                        :
+                        <>
+                              <InputCheckbox />
+                              <Text className="flex-1">Fazer Compras da Semana</Text>
+                              <Container as="div" className="flex gap-1">
+                                    <Button variant="icon_secondary" size="icon"><FaTrash /></Button>
+                                    <Button variant="icon_primary" size="icon" onClick={handleEditTask}><FaPencilAlt /></Button>
+                              </Container>
+                        </>
+                  }
             </Card>
       )
 }
